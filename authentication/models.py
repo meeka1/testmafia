4181aed448f6b8eda1b5b3ae388a18541aa09002
Mashkhura Eshmatova <eshmatovamashkhura@gmail.com>
2022-12-23 18:26:34 +0500

    hopefully last commit

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,9 @@
+import uuid
 from django.db import models
 
+
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     username = models.CharField(max_length=70, blank=False)
     email = models.CharField(max_length=200,blank=False)
     password = models.CharField(max_length=50)
